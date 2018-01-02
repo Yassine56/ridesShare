87e735283f9fc718b3339846455e376342b86876
Abouelouafa Yassine <swift@Abouelouafas-MacBook-Air.local>
2018-01-02 13:36:34 -0500

    User/Driver login with email implemented with Firebase

--- RidesShare/Controller/LoginVC.swift
+++ RidesShare/Controller/LoginVC.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
-
+import Firebase
 class LoginVC: UIViewController {
-
+    @IBOutlet var segmtedControl: UISegmentedControl!
+    @IBOutlet var authBtn: RoundshadowanimButton!
+    @IBOutlet var passwordTextField: RoundedCornerTextField!
+    @IBOutlet var emailTextFiel: RoundedCornerTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         view.bindToKeyBoard()
@@ -27,7 +30,64 @@ class LoginVC: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
-
+   
+    @IBAction func authbtnWasPressed(_ sender: Any) {
+        if ( emailTextFiel.text != nil && passwordTextField.text != nil){
+            authBtn.animateButton(shouldLoad: true, withMessage: nil)
+            self.view.endEditing(true)
+            if let email = emailTextFiel.text , let password = passwordTextField.text {
+                Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
+                    if error == nil {
+                        if let user = user {
+                            if self.segmtedControl.selectedSegmentIndex == 0 {
+                                let userData = ["provider": user.providerID] as [String: Any]
+                                DataService.instance.createFireBaseDBUser(uid: user.uid, userData: userData, isDriver: false)
+                            }else {
+                                let userData = ["provider": user.providerID, "userIsDriver": true, "isPickUpModeEnabled": false, "driverIsOnATrip": false] as [String: Any]
+                                DataService.instance.createFireBaseDBUser(uid: user.uid, userData: userData, isDriver: true)
+                            }
+                        }
+                        print("user signed up successfully using Firebase")
+                        self.dismiss(animated: true, completion: nil)
+                    } else {
+                        if let errorCode = AuthErrorCode(rawValue: error!._code) {
+                            switch errorCode {
+                            case .wrongPassword :
+                                print("wrong password or email already in use")
+                                self.view.endEditing(false)
+                                self.authBtn.animateButton(shouldLoad: false, withMessage: "Sign up/ login")
+                            default :
+                                print("an error has occured please try again")
+                
+                        }
+                        }
+                        
+                        Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+                            if error != nil {
+                                print(error!.localizedDescription)
+                            } else {
+                                if let user = user {
+                                    
+                                    if self.segmtedControl.selectedSegmentIndex == 0 {
+                                        let userData = ["provider": user.providerID] as [String: Any]
+                                        DataService.instance.createFireBaseDBUser(uid: user.uid, userData: userData, isDriver: false)
+                                    }else {
+                                        let userData = ["provider": user.providerID, "userIsDriver": true, "isPickUpModeEnabled": false, "driverIsOnATrip": false] as [String: Any]
+                                        DataService.instance.createFireBaseDBUser(uid: user.uid, userData: userData, isDriver: true)
+                                    }
+                                    
+                                }
+                                print("new user was successfully created")
+                                self.dismiss(animated: true, completion: nil )
+                            }
+                        })
+                    }
+                })
+            }
+            
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
