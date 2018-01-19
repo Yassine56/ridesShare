aa42a052f82e33063b7273a752a473f566bea8c2
Abouelouafa Yassine <swift@Abouelouafas-MacBook-Air.local>
2018-01-19 12:50:13 -0500

    destination text fied + tableview

--- RidesShare/Controller/HomeVC.swift
+++ RidesShare/Controller/HomeVC.swift
@@ -14,11 +14,14 @@ import Firebase
 
 class HomeVC: UIViewController,MKMapViewDelegate {
 
+    @IBOutlet var destinationTextField: UITextField!
+    @IBOutlet var centerbtnoutlet: UIButton!
     @IBOutlet var mapview: MKMapView!
     @IBOutlet var actionbutton: RoundshadowanimButton!
     var delegate: CenterVCdelegate?
     var manager: CLLocationManager?
     var regionRedius: CLLocationDistance = 1000
+    var tableView = UITableView()
     
     func checkLocationAuthStatus(){
         if CLLocationManager.authorizationStatus() == .authorizedAlways {
@@ -86,6 +89,7 @@ class HomeVC: UIViewController,MKMapViewDelegate {
     
     @IBAction func centerButtonWasPressed(_ sender: Any) {
         centerMapOnUserLocation()
+        centerbtnoutlet.fadeTo(alphaValue: 0.0, duration: 0.2)
     }
     
     @IBAction func actionbuttonpressed(_ sender: Any) {
@@ -93,7 +97,8 @@ class HomeVC: UIViewController,MKMapViewDelegate {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        tableView.delegate = self
+        destinationTextField.delegate = self
         manager = CLLocationManager()
         manager?.delegate = self
         manager?.desiredAccuracy = kCLLocationAccuracyBest
@@ -136,7 +141,9 @@ class HomeVC: UIViewController,MKMapViewDelegate {
         }
     }
 
-    
+    func mapView(_ mapView: MKMapView, regionWillChangeAnimated animated: Bool) {
+        centerbtnoutlet.fadeTo(alphaValue: 1.0, duration: 0.2)
+    }
 }
 extension HomeVC: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
@@ -148,9 +155,70 @@ extension HomeVC: CLLocationManagerDelegate {
         }
     }
 }
+extension HomeVC: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField == destinationTextField {
+        tableView.frame = CGRect(x: 20, y: view.frame.height, width: view.frame.width - 40, height: view.frame.height - 170)
+        tableView.layer.cornerRadius = 5.0
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "LocationCell")
+            tableView.delegate = self
+            tableView.dataSource = self
+            tableView.tag = 18
+            tableView.rowHeight = 60
+            view.addSubview(tableView)
+            animatetableview(shouldShow: true)
+        }
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == destinationTextField {
+            // performserach()
+            view.endEditing(true)
+        }
+        return true
+    }
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+    }
+    func animatetableview(shouldShow: Bool){
+        if shouldShow {
+            UIView.animate(withDuration: 0.2, animations: {
+                self.tableView.frame = CGRect(x: 20, y: 170, width: self.view.frame.width - 40, height: self.view.frame.height - 170)
+            })
+        }else {
+            UIView.animate(withDuration: 0.2, animations: {
+                self.tableView.frame = CGRect(x: 20, y: self.view.frame.height, width: self.view.frame.width - 40, height: self.view.frame.height - 170)
+            }, completion: { (finished) in
+                if finished {
+                    for subview in self.view.subviews {
+                        if subview.tag == 18 {
+                            subview.removeFromSuperview()
+                        }
+                    }
+                }
+            })
+        }
+    }
+}
 
 
-
-
+extension HomeVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("row selected")
+    }
+}
 
 
